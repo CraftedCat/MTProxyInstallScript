ec650ff79b645fc9bbeb88fa4ae14aac61cc705f
CraftedCat <38719532+CraftedCat@users.noreply.github.com>
2018-06-28 10:06:18 +0300

    Update OS detect

--- mtpis.sh
+++ mtpis.sh
@@ -1,15 +1,64 @@
 #!/bin/bash
 export DIR="/opt"
 export CronFile="/etc/cron.daily/mtproxy-multi"
+DEB_PACKAGE_NAME="htop curl git build-essential openssl libssl-dev zlib1g-dev"
+YUM_PACKAGE_NAME="htop curl git openssl-devel zlib-devel"
+YUM_PACKAGE_GROUP_NAME="Development Tools"
+if cat /etc/*release | grep ^NAME | grep CentOS; then
+    echo "==============================================="
+    echo "Installing packages $YUM_PACKAGE_NAME on CentOS"
+    echo "==============================================="
+    yum install -y $YUM_PACKAGE_NAME
+    yum groupinstall -y $YUM_PACKAGE_GROUP_NAME
+ elif cat /etc/*release | grep ^NAME | grep Red; then
+    echo "==============================================="
+    echo "Installing packages $YUM_PACKAGE_NAME on RedHat"
+    echo "==============================================="
+    yum install -y $YUM_PACKAGE_NAME
+    yum groupinstall -y $YUM_PACKAGE_GROUP_NAME
+ elif cat /etc/*release | grep ^NAME | grep Fedora; then
+    echo "================================================"
+    echo "Installing packages $YUM_PACKAGE_NAME on Fedorea"
+    echo "================================================"
+    yum install -y $YUM_PACKAGE_NAME
+    yum groupinstall -y $YUM_PACKAGE_GROUP_NAME
+ elif cat /etc/*release | grep ^NAME | grep Ubuntu; then
+    echo "==============================================="
+    echo "Installing packages $DEB_PACKAGE_NAME on Ubuntu"
+    echo "==============================================="
+    apt-get update
+    apt-get install -y $DEB_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Debian ; then
+    echo "==============================================="
+    echo "Installing packages $DEB_PACKAGE_NAME on Debian"
+    echo "==============================================="
+    apt-get update
+    apt-get install -y $DEB_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Mint ; then
+    echo "============================================="
+    echo "Installing packages $DEB_PACKAGE_NAME on Mint"
+    echo "============================================="
+    apt-get update
+    apt-get install -y $DEB_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Knoppix ; then
+    echo "================================================="
+    echo "Installing packages $DEB_PACKAGE_NAME on Kanoppix"
+    echo "================================================="
+    apt-get update
+    apt-get install -y $DEB_PACKAGE_NAME
+ else
+    echo "OS NOT DETECTED, couldn't install package $PACKAGE"
+    exit 1;
+ fi
+
+
+clear
 BOLD='\033[1m'       #  ${BOLD}
 LGREEN='\033[1;32m'     #  ${LGREEN}
 LBLUE='\033[1;34m'     #  ${LBLUE}
 BGGREEN='\033[42m'     #  ${BGGREEN}
 BGGRAY='\033[47m'     #  ${BGGRAY}
 BREAK='\033[m'       #  ${BREAK}
-clear
-apt-get update && apt-get install htop curl git build-essential openssl libssl-dev zlib1g-dev -y
-clear
 echo -en "\n${BOLD} Script install required packages, MTProto Proxy, startup script${BREAK}\n\n"
 cd $DIR && git clone https://github.com/TelegramMessenger/MTProxy.git && cd MTProxy && make
 cd $DIR/MTProxy/objs/bin && curl -s https://core.telegram.org/getProxySecret -o proxy-secret && curl -s https://core.telegram.org/getProxyConfig -o proxy-multi.conf
