1b326b48e147890be1801024933650faaa755caa
CraftedCat <38719532+CraftedCat@users.noreply.github.com>
2019-11-26 17:52:24 +0300

    up

--- mtpup.sh
+++ mtpup.sh
@@ -11,12 +11,16 @@ BGGREEN='\033[42m'     #  ${BGGREEN}
 BGGRAY='\033[47m'     #  ${BGGRAY}
 BREAK='\033[m'       #  ${BREAK}
 
+systemctl stop mtproxy
+
 echo -en "\n${BOLD} Script Update MTProto Proxy Files${BREAK}\n\n"
 cd $DIR && rm -rf MTProxy && git clone https://github.com/TelegramMessenger/MTProxy && cd MTProxy && make
 cd $DIR/MTProxy/objs/bin && curl -s https://core.telegram.org/getProxySecret -o proxy-secret && curl -s https://core.telegram.org/getProxyConfig -o proxy-multi.conf
 echo -en "${BOLD}Iptables rules file...${BREAK}\n\n"
 echo "iptables -A INPUT -j ACCEPT -p tcp -m tcp --dport 6443" > ${BINDIR}/iptables.sh
-systemctl daemon-reload && systemctl restart mtproxy
+
+systemctl start mtproxy
+
 echo -e  "===================================\n"
 echo -en "${LGREEN}Update Complete!${BREAK}\n"
 echo -en "check status ${BOLD}systemctl status mtproxy${BREAK}\n"
